574bc1ec0ee623f0155efd29a55f000bc186e981
myziani <myriamziani20@gmail.com>
2026-05-22 18:58:29 +0200

    ajout de makefile

--- test.sh
+++ test.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# test.sh - Compile et lance le programme.
-# A lancer depuis le dossier bibliocy-main :
-#     wsl bash test.sh     (depuis Windows)
-#     ./test.sh            (depuis WSL ou Linux)
-
-gcc -Wall -std=c99 -O2 main.c livre.c utilisateur.c emprunt.c -o bibliotech
-./bibliotech
+# Compile (uniquement ce qui a change) puis lance le programme.
+make run
